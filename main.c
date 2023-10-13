68602979b8f408a01e6f441c77ebe6bf89d820cc
Gui Yan-Zhong <guiyanzhong@gmail.com>
2023-10-13 10:11:56 +0800

    Translate comments into English.

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 
 #define PI 3.14159265
 #define SAMPLE_RATE 44100
-#define CHANNELS 2
-#define DURATION 5  // 时间长度（秒）
+#define CHANNELS 1
+#define DURATION 5  // Time in seconds
 #define AMPLITUDE 0.7
 #define FREQUENCY 440
 
@@ -22,34 +22,34 @@ int main() {
     unsigned int samples_played = 0;
     unsigned int total_samples = sample_rate * DURATION;
 
-    // 初始化PCM设备
+    // Initialize PCM device.
     err = snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
     if (err < 0) {
         printf("Cannot open PCM device: %s\n", snd_strerror(err));
         return -1;
     }
 
-    // 分配并初始化硬件参数对象
+    // Allocate & initialize hardware parameter object.
     snd_pcm_hw_params_malloc(&params);
     snd_pcm_hw_params_any(pcm_handle, params);
 
-    // 设置硬件参数
+    // Set hardware parameters.
     snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
     snd_pcm_hw_params_set_format(pcm_handle, params, SND_PCM_FORMAT_S16_LE);
     snd_pcm_hw_params_set_channels(pcm_handle, params, channels);
     snd_pcm_hw_params_set_rate_near(pcm_handle, params, &sample_rate, 0);
 
-    // 应用硬件参数
+    // Apply hardware parameters.
     snd_pcm_hw_params(pcm_handle, params);
 
-    // 获取缓冲区和周期大小
+    // Get buffer & period sizes.
     snd_pcm_hw_params_get_buffer_size(params, &buffer_size);
     snd_pcm_hw_params_get_period_size(params, &period_size, 0);
 
-    // 分配缓冲区内存
-    buffer = (char *)malloc(period_size * channels * 2);  // 2字节/样本
+    // Allocate buffer memory.
+    buffer = (char *)malloc(period_size * channels * 2);  // 2 bytes per sample.
 
-    // 生成正弦波音频数据并写入PCM设备
+    // Generate sine wave data & write to PCM device.
     while (samples_played < total_samples) {
         int i, j;
         short *ptr = (short *)buffer;
@@ -58,15 +58,15 @@ int main() {
             double value = AMPLITUDE * sin(2 * PI * FREQUENCY * (samples_played + i) / sample_rate);
 
             for (j = 0; j < channels; j++) {
-                *ptr++ = (short)(value * 32767);  // 将样本值转换为16位有符号整数
+                *ptr++ = (short)(value * 32767);  // Convert sample value to 16-bit signed integer.
             }
         }
 
-        // 写入PCM设备
+        // Write to PCM device.
         err = snd_pcm_writei(pcm_handle, buffer, period_size);
         if (err == -EPIPE) {
             printf("Underrun occurred\n");
-            snd_pcm_prepare(pcm_handle);  // 处理数据包未完成的情况
+            snd_pcm_prepare(pcm_handle);  // Handle unfinished data.
         } else if (err < 0) {
             printf("Error writing to PCM device: %s\n", snd_strerror(err));
             break;
@@ -75,7 +75,7 @@ int main() {
         samples_played += period_size;
     }
 
-    // 清理资源并关闭PCM设备
+    // Clean up resources & close PCM device.
     free(buffer);
     snd_pcm_drain(pcm_handle);
     snd_pcm_close(pcm_handle);
