b29e1d8b0afffde414e7df55b4e9389e83571658
Gui Yan-Zhong <guiyanzhong@gmail.com>
2023-10-15 15:57:47 +0800

    Make it a text-to-morse tool.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Morse Code Tool for Linux
+
+This is a command-line tool for playing with Morse Codes.
--- main.c
+++ main.c
@@ -1,27 +1,25 @@
+#include <getopt.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
 #include <alsa/asoundlib.h>
 
-#define PI 3.14159265
-#define SAMPLE_RATE 44100
-#define CHANNELS 1
-#define DURATION 5  // Time in seconds
 #define AMPLITUDE 0.7
 #define FREQUENCY 440
 
-int main() {
-    int err;
-    snd_pcm_t *pcm_handle;
-    snd_pcm_hw_params_t *params;
-    unsigned int sample_rate = SAMPLE_RATE;
-    unsigned int channels = CHANNELS;
-    snd_pcm_uframes_t buffer_size;
-    snd_pcm_uframes_t period_size;
-    char *buffer;
-    unsigned int samples_played = 0;
-    unsigned int total_samples = sample_rate * DURATION;
+int err;
+snd_pcm_t *pcm_handle;
+snd_pcm_hw_params_t *params;
+unsigned int frequency = 440;
+unsigned int sample_rate = 44100;
+unsigned int channels = 1;
+snd_pcm_uframes_t buffer_size;
+snd_pcm_uframes_t period_size;
+char *buffer;
 
+int init()
+{
     // Initialize PCM device.
     err = snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
     if (err < 0) {
@@ -49,15 +47,31 @@ int main() {
     // Allocate buffer memory.
     buffer = (char *)malloc(period_size * channels * 2);  // 2 bytes per sample.
 
+    return 0;
+}
+
+void cleanup()
+{
+    free(buffer);
+    snd_pcm_drain(pcm_handle);
+    snd_pcm_close(pcm_handle);
+    snd_pcm_hw_params_free(params);
+}
+
+void beep(double duration)
+{
+    snd_pcm_prepare(pcm_handle);
+
+    unsigned int samples_played = 0;
+    unsigned int total_samples = sample_rate * duration;
+
     // Generate sine wave data & write to PCM device.
+    double pi = 3.14159265;
     while (samples_played < total_samples) {
-        int i, j;
         short *ptr = (short *)buffer;
-
-        for (i = 0; i < period_size; i++) {
-            double value = AMPLITUDE * sin(2 * PI * FREQUENCY * (samples_played + i) / sample_rate);
-
-            for (j = 0; j < channels; j++) {
+        for (int i = 0; i < period_size; i++) {
+            double value = AMPLITUDE * sin(2 * pi * FREQUENCY * (samples_played + i) / sample_rate);
+            for (int j = 0; j < channels; j++) {
                 *ptr++ = (short)(value * 32767);  // Convert sample value to 16-bit signed integer.
             }
         }
@@ -75,11 +89,76 @@ int main() {
         samples_played += period_size;
     }
 
+    usleep(duration * 1500000);
+}
+
+int main(int argc, char* argv[]) {
+    // Text to convert to Morse code.
+    char text[256] = "CQ CQ DE BH4FYQ K";
+
+    int c = 0;
+    while((c = getopt(argc, argv, "hf:t:")) != -1) {
+        switch(c) {
+        case 'h':
+            printf("Usage: %s -f 600 -t \"CQ CQ DE ...\"\n", argv[0]);
+            return 0;
+        case 'f':
+            strncpy(text, optarg, 256);
+            break;
+        case 't':
+            strncpy(text, optarg, 256);
+            break;
+        default:
+            return 0;
+        }
+    }
+
+    printf("%s\n", text);
+
+    // Init.
+    if ((err = init()))
+        return err;
+
+    // Morse codes for alphabets
+    char *morseAZ[] = {
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",   // A-I
+        ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", // J-R
+        "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."         // S-Z
+    };
+
+    // Morse codes for digits
+    char *morse09[] = {
+        "-----", ".----", "..---", "...--", "....-",                     // 0-4
+        ".....", "-....", "--...", "---..", "----."                      // 5-9
+    };
+
+    // Beep.
+    for (int i = 0; i < strlen(text); i ++) {
+        char c = text[i];
+        char *code = " ";
+        if (c >= 'A' && c <= 'Z')
+            code = morseAZ[c - 'A'];
+        if (c >= 'a' && c <= 'z')
+            code = morseAZ[c - 'a'];
+        if (c >= '0' && c <= '9')
+            code = morse09[c - '0'];
+        printf("%c %s\n", c, code);
+        fflush(stdout);
+        for (int i = 0; i < strlen(code); i ++) {
+            if (code[i] == '-')
+                beep(0.2);
+            else if (code[i] == '.')
+                beep(0.07);
+            else {
+                usleep(100000);
+            }
+        }
+        usleep(200000);
+    }
+    printf("\n");
+
     // Clean up resources & close PCM device.
-    free(buffer);
-    snd_pcm_drain(pcm_handle);
-    snd_pcm_close(pcm_handle);
-    snd_pcm_hw_params_free(params);
+    cleanup();
 
     return 0;
 }
